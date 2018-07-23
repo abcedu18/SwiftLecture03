5a94e659aeeacfc7a4cdf8460c4928e11c7319d3
abcedu18 <1450330214@qq.com>
2018-07-23 16:40:40 +0800

    修改 2018.7.23下午

--- SwiftLecture03/ViewController.swift
+++ SwiftLecture03/ViewController.swift
@@ -1,32 +1,55 @@
-//
 //  ViewController.swift
 //  SwiftLecture03
 //
 //  Created by 走着唱着 on 2018/7/22.
 //  Copyright © 2018年 anshangyuan. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var lbName: UILabel!
     
-    @IBOutlet weak var btnConfirm: UIButton!
+    @IBOutlet weak var btnCofirm: UIButton!
+    
+    @IBOutlet weak var switch1: UISwitch!
+    
+   
+    @IBOutlet weak var stepper1: UIStepper!
+    
+
+    @IBOutlet weak var slider1: UISlider!
     
     @IBOutlet weak var touchView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
     @IBAction func click(_ sender: Any) {
         lbName.text="Tao Feng"
-//        
-//      let button = sender as! UIButton;
-//        
-//      button.setTitle("OK", for: UIControlState.normal)
         
+        let btnConfirm = sender as! UIButton
+        
+        btnConfirm.setTitle("OK", for: UIControlState.normal)
     }
+    @IBAction func switchChanged(_ sender: Any) {
+        let switch1 = sender as! UISwitch
+        print(switch1.isOn)
+    }
+    
+
+    @IBAction func stepperChanged(_ sender: Any) {
+        
+        self.slider1.value = Float(self.stepper1.value)
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        let doubleValue = self.slider1.value
+        print(doubleValue)
+    }
+  
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("touchesBegan")
@@ -46,8 +69,6 @@ class ViewController: UIViewController {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
       print("touchesEnded")
-       
     }
-    
 }
 
